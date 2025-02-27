0c082273d24dc54209959ed0fef57a10446ee93b
Nils Nieuwejaar <nils@oxidecomputer.com>
2025-02-27 13:01:17 +0000

    fetch absl

--- .github/buildomat/jobs/illumos.sh
+++ .github/buildomat/jobs/illumos.sh
@@ -23,8 +23,14 @@ set -o xtrace
 
 export PATH=$PATH:/home/build/.local/bin
 banner "packages"
+
+# Packages currently pre-built on s3.  Ideally this will be part of the
+# helios repo so they can be "pkg install"ed.
 wget https://oxide-tofino-build.s3.us-west-2.amazonaws.com/boost.tar.gz
 (cd / ; pfexec tar xfvz /work/oxidecomputer/tofino-sde/boost.tar.gz)
+wget https://oxide-tofino-build.s3.us-west-2.amazonaws.com/abseil.tar.gz
+(cd / ; pfexec tar xfvz /work/oxidecomputer/tofino-sde/abseil.tar.gz)
+
 echo pfexec pkg install cmake gcc12 bdw-gc
 pwd
 python -m pip install jsl pyinstaller
