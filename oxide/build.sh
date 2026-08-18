59d68534c4e9067e2ebe3538f4c1d9d1760403ba
Cory Zimmerman <coryzimmerman93@gmail.com>
2026-08-18 12:20:29 -0500

    shellcheck fixes in oxide/build.sh

--- oxide/build.sh
+++ oxide/build.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-if [ `uname -s` == SunOS ]; then
+if [ "$(uname -s)" == SunOS ]; then
     export ILLUMOS=1
 else
     export ILLUMOS=0
@@ -13,14 +13,14 @@ export PATH=${PATH}:~/.local/bin
 
 function get_firmware() {
     FW_DIR=${SDE}/install/share/tofino_sds_fw/credo
-    mkdir -p $FW_DIR
+    mkdir -p "$FW_DIR"
     wget -P /tmp https://oxide-tofino-build.s3.us-west-2.amazonaws.com/serdes_fw.tar.gz
-    (cd $FW_DIR ; tar xvfz /tmp/serdes_fw.tar.gz)
+    (cd "$FW_DIR" ; tar xvfz /tmp/serdes_fw.tar.gz)
 }
 
 function patch_abseil() {
     (
-        cd ${SDE}/install/include/absl/container/internal/
+        cd "$SDE"/install/include/absl/container/internal/
 	# if layout.h hasn't already been patched, do it now.
         grep -q "std::free" layout.h || cat << EOF | patch -p1 layout.h
 --- layout.h.orig	2025-02-20 21:52:10.936645134 +0000
@@ -41,14 +41,14 @@ EOF
 function prework() {
     if [ $ILLUMOS -eq 1 ]; then
         echo Wrapping wrap_libport_mgr_hw
-        (cd ${SDE}/oxide/wrap_libport_mgr_hw ; gmake clean; gmake install)
+        (cd "$SDE"/oxide/wrap_libport_mgr_hw ; gmake clean; gmake install)
     else
         patch_abseil
     fi
 
     RAPIDJSON_DIR=${SDE}/oxide/rapidjson
-    if [ ! -d $RAPIDJSON_DIR ]; then
-        (cd ${SDE}/oxide ; git clone https://github.com/Tencent/rapidjson.git)
+    if [ ! -d "$RAPIDJSON_DIR" ]; then
+        (cd "$SDE"/oxide ; git clone https://github.com/Tencent/rapidjson.git)
     fi
 }
 
@@ -57,7 +57,6 @@ function configure_build {
         # We only want to build the sidecar code on helios
         BSP=OFF
         LINKER_FLAGS=""
-        BOOST_ROOT=""
         BOOST_DIR=/usr/include
         BOOST_STATIC_RUNTIME='OFF'
         CXX_FLAGS="-I${SDE}/oxide/rapidjson/include"
@@ -93,8 +92,8 @@ function configure_build {
         fi
     fi
 
-    cd ${SDE}/build
-    cmake $SDE \
+    cd "$SDE"/build
+    cmake "$SDE" \
         -DASIC=ON  \
         -DTOFINO=OFF \
         -DTOFINO2=ON \
@@ -112,16 +111,15 @@ function configure_build {
         -DBF-PYTHON=OFF \
         -DKERNEL-MODULES=OFF \
         -DBSP=$BSP \
-        -DRAPIDJSON_DIR=$RAPIDJSON_DIR/include \
+        -DRAPIDJSON_DIR="$RAPIDJSON_DIR"/include \
         -DP4C_USE_PREINSTALLED_ABSEIL=ON \
         -DP4C_USE_PREINSTALLED_PROTOBUF=OFF \
-        ${BOOST_ROOT} \
         -DBoost_INCLUDE_DIRS=${BOOST_DIR} \
         -DBoost_USE_STATIC_RUNTIME=${BOOST_STATIC_RUNTIME} \
         -DBoost_USE_STATIC_LIBS='ON' \
         -DCMAKE_BUILD_TYPE='Release' \
         -DCMAKE_LINKER='lld' \
-        -DCMAKE_INSTALL_PREFIX=$SDE/install \
+        -DCMAKE_INSTALL_PREFIX="$SDE"/install \
         -DCMAKE_CXX_FLAGS="$CXX_FLAGS" \
         -DCMAKE_EXE_LINKER_FLAGS="$LINKER_FLAGS" \
         -DCMAKE_C_FLAGS="$C_FLAGS" \
@@ -129,35 +127,36 @@ function configure_build {
 }
 
 function build {
-    cd $SDE/build
-    gmake -j ${JOBS} install
+    cd "$SDE"/build
+    gmake -j "$JOBS" install
     if [ $ILLUMOS -eq 1 ]; then
 	get_firmware
     else
-        (cd ${SDE}/oxide/remote_model ; make install)
+        (cd "$SDE"/oxide/remote_model ; make install)
     fi
 }
 
 
 function usage() {
-    printf "$0 [-h] [-v <version>] [-j <jobs>]\n"
+    printf "%s [-h] [-v <version>] [-j <jobs>]\n" "$0"
     printf "    -h \tThis message\n"
     printf "    -j \tTell (g)make how many jobs to spawn\n"
 }
 
-export SDE=`git rev-parse --show-toplevel`
-echo Building SDE at git root: ${SDE}
-mkdir ${SDE}/build || echo ${SDE}/build already exists
+export SDE
+SDE=$(git rev-parse --show-toplevel)
+echo Building SDE at git root: "$SDE"
+mkdir "$SDE"/build || echo "$SDE"/build already exists
 
 export JOBS=8
 while getopts hs:j:v: opt; do
-    if [ $opt == "h" ]; then
-        usage $0
+    if [ "$opt" == "h" ]; then
+        usage "$0"
         exit 0
-    elif [ $opt == "j" ]; then
+    elif [ "$opt" == "j" ]; then
         JOBS=$OPTARG
     else
-        usage $0
+        usage "$0"
         exit 1
     fi
 done
